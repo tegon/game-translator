e23f6a3829ad083213e2ec1d4394830c1d5fe446
Leonardo Tegon <leonardotegon@clickjogos.com.br>
2013-07-15 14:20:34 -0300

    initial commit

--- game-translator/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-GameTranslator::Application.config.secret_token = '0627eec18a97edbf41169d58ad776f637766711cb21ab29bae519923aefe762753c3ad1114626f5bf00dd2648745def80915af68e4f6218dd356e2e63544e3c3'
