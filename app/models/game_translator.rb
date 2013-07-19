917f0eb3b4876b1e7fcfebbdabbf65009d8a7cd9
Leonardo Tegon <leonardotegon@clickjogos.com.br>
2013-07-19 13:47:42 -0300

    migration

--- app/models/game_translator.rb
+++ app/models/game_translator.rb
@@ -1,5 +1,2 @@
 module GameTranslator
-  def self.table_name_prefix
-    'game_translator_'
-  end
 end
--- app/models/game_translator/user.rb
+++ app/models/game_translator/user.rb
@@ -1,8 +1,5 @@
 class GameTranslator::User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  # attr_accessible :title, :body
+         :rememberable, :trackable, :validatable
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
 end
