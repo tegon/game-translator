c7b3a34fcc5b0840290538f410f19940648ffb37
leotegon93 <leonardotegon@clickjogos.com.br>
2013-07-21 17:10:14 -0300

    colocando campos no form

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,9 @@ class GamesController < ApplicationController
 	def edit
 		@game = ClickJogos::Game.find(params[:id])
 	end
+	
+	def update
+		p '-------------------------------------------------'
+		p params[:name_en]
+	end
 end
