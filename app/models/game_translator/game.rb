57c39c2507fd9abef2ac705eb1f366eb8652ee42
leotegon93 <leonardotegon@clickjogos.com.br>
2013-07-22 22:21:21 -0300

    adicionei os escopos

--- app/models/game_translator/game.rb
+++ app/models/game_translator/game.rb
@@ -2,6 +2,9 @@ module GameTranslator
 	class Game < ActiveRecord::Base
 		# scopes
 		scope :not_translated, conditions: { translated: false }
+		scope :translated, conditions: { translated: true }
+		scope :not_revised, conditions: { revised: false }
+		scope :revised, conditions: { revised: true }
 		scope :random, order: 'RAND()'
 	end
 end
