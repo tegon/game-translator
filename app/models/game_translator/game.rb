866219845a598b125c9d558fcd373ae7efaefd12
Leonardo Tegon <leonardotegon@clickjogos.com.br>
2013-08-09 11:40:30 -0300

    arrumei o problema da migration

--- app/models/game_translator/game.rb
+++ app/models/game_translator/game.rb
@@ -3,7 +3,7 @@ module GameTranslator
     # translated fields
     translates :name, :short_description, :long_description, :wide_description, 
     	:instructions
-    globalize_accessors locales: GameTranslator::Language.abbreviations
+    globalize_accessors locales: GameTranslator::Language.abbreviations if ActiveRecord::Base.connection.table_exists? 'languages'
     
     # relationship 
     has_many :game_translations
