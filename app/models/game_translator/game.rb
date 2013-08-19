f2eb96b64ca620125c0c5d12585fb7585163d4a2
Leonardo Tegon <leonardotegon@clickjogos.com.br>
2013-08-19 14:21:56 -0300

    removi validação que não estava funcionando

--- app/models/game_translator/game.rb
+++ app/models/game_translator/game.rb
@@ -11,14 +11,6 @@ module GameTranslator
     # validates
     validates :status, presence: true, inclusion: { in: %w(not_translated translated translating) }
 
-    
-    GameTranslator::Language.codes.each do |locale|
-      validates "name_#{ locale }".to_sym, presence: true, allow_blank: false 
-      validates "short_description_#{ locale }".to_sym, presence: true, allow_blank: false 
-      validates "long_description_#{ locale }".to_sym, presence: true, allow_blank: false 
-      validates "instructions_#{ locale }".to_sym, presence: true, allow_blank: false
-    end
-
     # scopes
     scope :not_translated, conditions: { status: 'not_translated' }
     scope :translated, conditions: { status: 'translated' }
