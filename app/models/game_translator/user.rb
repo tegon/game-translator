266d2b86bdbe489d0201639317d66fd676ecc18d
Leonardo Tegon <leonardotegon@clickjogos.com.br>
2013-08-21 14:03:57 -0300

    ajuste no modelo User

--- app/models/game_translator/user.rb
+++ app/models/game_translator/user.rb
@@ -1,30 +1,32 @@
-class GameTranslator::User < ActiveRecord::Base
-  # devise
-  devise :database_authenticatable, :registerable,
-         :rememberable, :trackable, :validatable
-  
-  # relationship  
-  has_many :game_translations
-  has_many :reviews
+module GameTranslator
+  class User < ActiveRecord::Base
+    # devise
+    devise :database_authenticatable, :registerable,
+           :rememberable, :trackable, :validatable
+    
+    # relationship  
+    has_many :game_translations
+    has_many :reviews
 
-  # validates
-  validates :name, presence: true, allow_blank: false, length: { minimum: 3 }
-  validates :role, presence: true, inclusion: { in: %w(translator reviser) }
+    # validates
+    validates :name, presence: true, allow_blank: false, length: { minimum: 3 }
+    validates :role, presence: true, inclusion: { in: %w(translator reviser) }
 
-  # scopes
-  scope :translators, conditions: { role: 'translator' }
-  scope :revisers, conditions: { role: 'reviser' }
+    # scopes
+    scope :translators, conditions: { role: 'translator' }
+    scope :revisers, conditions: { role: 'reviser' }
 
-  # cancan roles
-  ROLES = %w[translator reviser]
+    # cancan roles
+    ROLES = %w[translator reviser]
 
-  def reviser?
-  	self.role == 'reviser'
+    def reviser?
+      self.role == 'reviser'
+    end
+
+    def translator?
+      self.role == 'translator'
+    end
+
+    self.per_page = 10
   end
-
-  def translator?
-  	self.role == 'translator'
-  end
-
-  self.per_page = 10
 end
