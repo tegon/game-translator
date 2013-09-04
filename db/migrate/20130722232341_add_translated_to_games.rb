7c01cdef06c1463b9edb50b77e3bdd8cdd5a4d59
Leonardo Tegon <leonardotegon@clickjogos.com.br>
2013-09-04 17:03:06 -0300

    really?

--- db/migrate/20130722232341_add_translated_to_games.rb
+++ db/migrate/20130722232341_add_translated_to_games.rb
@@ -1,5 +1,5 @@
 class AddTranslatedToGames < ActiveRecord::Migration
   def change
-  	add_column :games, :translated, :boolean, default: false
+  	add_column :games, :translated, :integer, default: 0
   end
 end
