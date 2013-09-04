7713b2fe8a9c2e5bc26b9093ecec1e1c83f04852
Leonardo Tegon <leonardotegon@clickjogos.com.br>
2013-09-04 17:12:00 -0300

    alterando seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
-require 'ffaker'
-
 50.times do |i|
-  Game.create(name: "Game #{ i }", short_description: Faker::Lorem.sentence, long_description: Faker::Lorem.paragraphs.join, instructions: "[mouse] #{ Faker::Name.name }")
+  Game.create(name: "Game #{ i }",
+    short_description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
+    tincidunt ut laoreet dolore magna aliquam erat volutpat.',
+    long_description: 'Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    instructions: "[mouse] Lorem ipsum dolor")
   puts "Game #{ i } criado!"
 end
