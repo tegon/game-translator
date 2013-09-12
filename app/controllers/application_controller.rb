6e9bcb0f0c1d3fa6ba79d1e3abb6799c4dbd343e
Tegon <leonardotegon@clickjogos.com.br>
2013-09-11 23:58:18 -0300

    iniciando internacionalizacao

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :authenticate_user!
-  
+  before_filter :set_locale
+
   rescue_from CanCan::AccessDenied do |exception|
     case current_user.role
     when 'translator'
@@ -10,5 +11,15 @@ class ApplicationController < ActionController::Base
     when 'reviser'
       redirect_to user_index_path
     end
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || ((lang = request.env['HTTP_ACCEPT_LANGUAGE']) && lang[/^[a-z]{2}/])
+    p '/'*100
+    p I18n.locale
+  end
+
+  def default_url_options(options={})
+    { :locale => I18n.locale == I18n.default_locale ? nil : I18n.locale  }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,37 @@
 GameTranslator::Application.routes.draw do
 
-  root to: 'game_translator/games#translate'
+  scope "(:locale)", :locale => /pt-BR|en|es/ do
+    root to: 'game_translator/games#translate'
 
-  devise_for :users, skip: [:registrations], class_name: 'GameTranslator::User', path_names: { 
-    sign_in: 'login', sign_out: 'logout' 
-    }, controllers: { 
-      registrations: 'game_translator/registrations', 
-      sessions: 'game_translator/sessions' 
-      }
- 
-  get 'users' => 'game_translator/users#index', as: :user_index
-  get 'users/new' => 'game_translator/users#new', as: :user_new
-  post 'users/create' => 'game_translator/users#create', as: :user_create
-  put 'users/:id' => 'game_translator/users#update', as: :user_update
-  get 'users/:id/edit' => 'game_translator/users#edit', as: :user_edit
-  delete 'users/:id' => 'game_translator/users#destroy', as: :user_destroy
-  
-  get 'translate' => 'game_translator/games#translate', as: :game_translate
-  put 'translate/update' => 'game_translator/games#update', as: :game_update
-  
-  get 'review' => 'game_translator/reviews#index', as: :review
-  get 'review/:id/edit' => 'game_translator/reviews#edit', as: :review_edit
-  put 'review/:id' => 'game_translator/reviews#update', as: :review_update
+    devise_for :users, skip: [:registrations], class_name: 'GameTranslator::User', path_names: { 
+      sign_in: 'login', sign_out: 'logout' 
+      }, controllers: { 
+        registrations: 'game_translator/registrations', 
+        sessions: 'game_translator/sessions' 
+        }
+   
+    get 'users' => 'game_translator/users#index', as: :user_index
+    get 'users/new' => 'game_translator/users#new', as: :user_new
+    post 'users/create' => 'game_translator/users#create', as: :user_create
+    put 'users/:id' => 'game_translator/users#update', as: :user_update
+    get 'users/:id/edit' => 'game_translator/users#edit', as: :user_edit
+    delete 'users/:id' => 'game_translator/users#destroy', as: :user_destroy
+    
+    get 'translate' => 'game_translator/games#translate', as: :game_translate
+    put 'translate/update' => 'game_translator/games#update', as: :game_update
+    
+    get 'review' => 'game_translator/reviews#index', as: :review
+    get 'review/:id/edit' => 'game_translator/reviews#edit', as: :review_edit
+    put 'review/:id' => 'game_translator/reviews#update', as: :review_update
 
 
-  get '/languages' => 'game_translator/languages#index', as: :languages
-  post '/languages' => 'game_translator/languages#create', as: :language_create
-  get '/languages/new' => 'game_translator/languages#new', as: :language_new
-  get '/languages/:id/edit' => 'game_translator/languages#edit', as: :language_edit
-  put '/languages/:id' => 'game_translator/languages#update', as: :language_update
-  delete '/languages/:id' => 'game_translator/languages#destroy', as: :language_destroy
+    get '/languages' => 'game_translator/languages#index', as: :languages
+    post '/languages' => 'game_translator/languages#create', as: :language_create
+    get '/languages/new' => 'game_translator/languages#new', as: :language_new
+    get '/languages/:id/edit' => 'game_translator/languages#edit', as: :language_edit
+    put '/languages/:id' => 'game_translator/languages#update', as: :language_update
+    delete '/languages/:id' => 'game_translator/languages#destroy', as: :language_destroy
 
-  get '/stats' => 'game_translator/stats#index', as: :stats
+    get '/stats' => 'game_translator/stats#index', as: :stats
+  end
 end
