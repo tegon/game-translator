b5845008a07d20ae3ff9b7888f87f72bcb9ad355
Leonardo Tegon <leonardotegon@clickjogos.com.br>
2013-09-16 16:15:42 -0300

    refactor no teste de translation

--- spec/models/game_translator/game_translation_spec.rb
+++ spec/models/game_translator/game_translation_spec.rb
@@ -1,48 +1,58 @@
 require 'spec_helper'
 
 describe GameTranslator::Game::Translation do
-  before do
-    @translation = create(:game_translator_game_translation)
-    @not_revised = create(:game_translator_game_translation, revised: false, review_id: nil)
-  end
+  let(:translation) { create(:game_translator_game_translation) }
 
   it 'has a valid generator' do
     create(:game_translator_game_translation).should be_valid
   end
 
-  it 'belongs to game' do
-    @translation.should respond_to :game
-  end
+  it { should respond_to :game }
 
-  it 'belongs to user' do
-    @translation.should respond_to :user
+  it { should respond_to :user }
+
+  it 'updates user' do
+    user = create(:game_translator_user)
+    translation.update_attribute(:user, user)
+    translation.user.should eql(user)
   end
 
   it 'belongs to review' do
-    @translation.should respond_to :review
+    translation.should respond_to :review
   end
 
-  describe 'not_revised scope' do
-    it 'has a scope to not revised translations' do
-      GameTranslator::Game::Translation.should respond_to :not_revised
-    end
-
-    it 'returns only not revised translations' do
-      not_revised_translations = GameTranslator::Game::Translation.not_revised
-      not_revised_translations.should_not include @translation
-      not_revised_translations.should include @not_revised
-    end
+  it 'updates review' do
+    review = create(:game_translator_review)
+    translation.update_attribute(:review, review)
+    translation.review.should eql(review)
   end
 
-  describe 'revised scope' do
-    it 'has a scope to revised translations' do
-      GameTranslator::Game::Translation.should respond_to :revised
+  context 'scopes' do
+    subject { GameTranslator::Game::Translation }
+
+    before do
+      @translation = create(:game_translator_game_translation)
+      @not_revised = create(:game_translator_game_translation, revised: false, review_id: nil)
     end
 
-    it 'returns only revised translations' do
-      revised_translations = GameTranslator::Game::Translation.revised
-      revised_translations.should_not include @not_revised
-      revised_translations.should include @translation
+    describe 'not_revised scope' do
+      it { should respond_to :not_revised }
+
+      it 'returns only not revised translations' do
+        not_revised_translations = GameTranslator::Game::Translation.not_revised
+        not_revised_translations.should_not include @translation
+        not_revised_translations.should include @not_revised
+      end
+    end
+
+    describe 'revised scope' do
+      it { should respond_to :revised }
+
+      it 'returns only revised translations' do
+        revised_translations = GameTranslator::Game::Translation.revised
+        revised_translations.should_not include @not_revised
+        revised_translations.should include @translation
+      end
     end
   end
 end
