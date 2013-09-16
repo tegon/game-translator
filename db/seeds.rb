504b3ec2a627a363b842924dca5f5be0cae4610f
Leonardo Tegon <leonardotegon@clickjogos.com.br>
2013-09-16 08:25:27 -0300

    alterei short_description do seed, estava muito grande

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,11 @@
-user = User.create(name: 'Reviser', email: 'reviser@domain.com', 
-                    password: '123123123', password_confirmation: '123123123', 
+user = User.create(name: 'Reviser', email: 'reviser@domain.com',
+                    password: '123123123', password_confirmation: '123123123',
                     role: 'reviser')
 puts "Reviser-#{user.id} criado!"
 
 5000.times do |i|
   Game.create(name: "Game #{ i }",
-    short_description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
-    tincidunt ut laoreet dolore magna aliquam erat volutpat.',
+    short_description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
     long_description: 'Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     instructions: "[mouse] Lorem ipsum dolor")
   puts "Game #{ i } criado!"
