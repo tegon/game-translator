538caad297cc911c97e7e10d608f25c2bb5fa519
Leonardo Tegon <leonardotegon@clickjogos.com.br>
2013-09-17 11:26:04 -0300

    adicionando forms e models com tradução

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
     I18n.locale = params[:locale] || I18n.default_locale
   end
 
-  def default_url_options(options={})
-    { :locale => I18n.locale == I18n.default_locale ? nil : I18n.locale  }
+  def default_url_options(options = {})
+    { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale  }
   end
 end
