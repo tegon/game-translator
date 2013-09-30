8d1f54070ce70fb2b52536d349513e7bdb286731
tegon <leonardotegon@clickjogos.com.br>
2013-09-30 19:34:34 -0300

    corrigindo seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-user = User.create(name: 'Reviser', email: 'reviser@domain.com',
+user = GameTranslator::User.create(name: 'Reviser', email: 'reviser@domain.com',
                     password: '123123123', password_confirmation: '123123123',
                     role: 'reviser')
 puts "Reviser-#{user.id} criado!"
 
 5000.times do |i|
-  Game.create(name: "Game #{ i }",
+  GameTranslator::Game.create(name: "Game #{ i }",
     short_description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
     long_description: 'Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     instructions: "[mouse] Lorem ipsum dolor")
